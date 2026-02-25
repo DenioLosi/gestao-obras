c7f0b16a3e36709068d66f6135753e80578192df
DenioLosi <denolis@gmail.com>
2026-02-24 21:40:42 -0300

    Create obras.js

---
+++ pages/obras.js
@@ -0,0 +1,8 @@
+export default function Obras() {
+  return (
+    <div style={{ padding: 24, fontFamily: "Arial" }}>
+      <h1>Obras</h1>
+      <p>Rota /obras criada. Próximo passo: listar obras e unidades.</p>
+    </div>
+  )
+}
